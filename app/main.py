712691d313e777f830be0e32d739cad7f276ceb5
herveRI <herve.irad@gmail.com>
2026-04-23 03:46:50 +0000

    Minor fixes

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my API - Successfuly CI/CD witn DG_Ocean"}
+    return {"message": "Welcome to my API"}
